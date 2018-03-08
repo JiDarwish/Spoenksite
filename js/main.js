62a10f017eafbfce0ce60bf60317f12d16f89e3b
IshaD <ishadijcks@gmail.com>
2018-03-08 09:17:57 +0100

    Formatting

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@ var calendar = {
 };
 
 $(document).ready(function () {
-    $('.smooth-goto').on('click', function() {
+    $('.smooth-goto').on('click', function () {
         $('html, body').animate({scrollTop: $(this.hash).offset().top - 50}, 1000);
         return false;
     });
